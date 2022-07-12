8ec185d8c8dcac0ff1a0eaef59ee1f74b9e025d6
Gage Miller <gagemiller155@gmail.com>
2022-07-11 22:01:03 -0400

    Add robot hardware

---
+++ src/robot_description/launch/isaac.launch.py
@@ -0,0 +1,107 @@
+import os
+from ament_index_python.packages import get_package_prefix, get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+
+    # Get Local Files
+    pkg_path = os.path.join(get_package_share_directory('robot_description'))
+    xacro_file = os.path.join(pkg_path, 'urdf', 'robots','examplo.urdf.xacro')
+    controllers_file = os.path.join(pkg_path, 'config', 'controllers.yaml')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description_xml = robot_description_config.toxml()
+    
+    # Create a robot_state_publisher node
+    description_params = {'robot_description': robot_description_xml, 'use_sim_time': True }
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[description_params]
+    )
+
+
+    # Starts ROS2 Control
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description_xml}, controllers_file],
+        output="screen",
+    )
+
+
+    # Starts ROS2 Control Joint State Broadcaster
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+    
+
+
+    # Starts ROS2 Control Diff Drive Controller
+    diff_drive_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_train_controller", "-c", "/controller_manager"],
+    )
+    diff_drive_controller_delay = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[diff_drive_controller_spawner],
+        )
+    )
+
+
+    # Start Rviz2 with basic view
+    rviz2_config_path = os.path.join(get_package_share_directory('robot_description'), 'config/isaac.rviz')
+    run_rviz2 = ExecuteProcess(
+        cmd=['rviz2', '-d', rviz2_config_path],
+        output='screen'
+    )
+    rviz2_delay = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[run_rviz2],
+        )
+    )
+
+
+    # Start Joystick Node
+    joy = Node(
+            package='joy', 
+            executable='joy_node', 
+            name='joy_node',
+            parameters=[{
+                'dev': '/dev/input/js0',
+                'deadzone': 0.3,
+                'autorepeat_rate': 20.0,
+            }])
+
+
+    # Start Teleop Node to translate joystick commands to robot commands
+    config_filepath = os.path.join(get_package_share_directory('teleop_twist_joy'), 'config/xbox.config.yaml')
+    joy_teleop = Node(
+        package='teleop_twist_joy', 
+        executable='teleop_node',
+        name='teleop_twist_joy_node', 
+        parameters=[config_filepath],
+        remappings={('/cmd_vel', '/diff_drive_train_controller/cmd_vel_unstamped')}
+        )
+
+
+    # Launch!
+    return LaunchDescription([
+        control_node,
+        node_robot_state_publisher,
+        joint_state_broadcaster_spawner,
+        diff_drive_controller_delay,
+        rviz2_delay,
+        joy,
+        joy_teleop
+    ])
